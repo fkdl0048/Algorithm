745598a9c6426c6114028823cd3ec043766d35f2
lee jeongan <fkdl000048@gmail.com>
2021-12-31 16:44:55 +0900

    15651 문제풀이 n과 m (3)

---
+++ 15651/15651.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#define MAX 9
+using namespace std;
+
+int n, m;
+int arr[MAX] = {0};
+
+void dfs(int cnt){
+    if(cnt == m){
+        for (int i = 0; i < m; i++){
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = 1; i <= n; i++){
+        arr[cnt] = i;
+        dfs(cnt + 1);
+    }
+}
+
+int main(){
+    cin >> n >> m;
+    dfs(0);
+}
