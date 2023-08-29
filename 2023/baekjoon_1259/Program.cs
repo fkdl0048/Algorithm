f95321e2a35d80350a546bf052eb2e6bf5d9d113
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-08-29 23:59:39 +0900

    Algorithm: Solve baekjoon-1259

---
+++ 2023/baekjoon_1259/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace baekjoon_1259
+{
+    public class Palindrome
+    {
+        public bool IsPalindrome(string str)
+        {
+            int len = str.Length;
+            for (int i = 0; i < len / 2; i++)
+            {
+                if (str[i] != str[len - i - 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Palindrome palindrome = new Palindrome();
+            while (true)
+            {
+                string str = System.Console.ReadLine();
+                if (str == "0")
+                    break;
+                if (palindrome.IsPalindrome(str))
+                    System.Console.WriteLine("yes");
+                else
+                    System.Console.WriteLine("no");
+            }
+        }
+    }
+}
