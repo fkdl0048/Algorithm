9ba0b9a5fe396f64c626ad6004d7b4490f5a3d98
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2024-05-17 14:57:04 +0900

    BOJ-EX: 5/17/2024, 2:57:02 PM

--- 10808번： 알파벳 개수/알파벳 개수.cpp
+++ 10808번： 알파벳 개수/알파벳 개수.cpp
@@ -13,16 +13,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// int main(){
+//     map<char, int> a;
+//     string s;
+//     cin >> s;
+
+//     for (int i = 0; i < s.length(); i++)
+//     {
+//         a[s[i]]++;
+//     }
+
+//     for (char i = 'a'; i <= 'z'; i++)
+//     {
+//         cout << a[i] << ' ';
+//     }
+// }
+
+int cnt[26];
 int main(){
     string s;
     cin >> s;
-    vector<int> v(26);
-    for (char c : s)
+    for (int i= 0; i < s.length(); i++)
     {
-        v[c - 'a']++;
+        cnt[s[i] - 'a']++;
     }
+
     for (int i = 0; i < 26; i++)
     {
-        cout << v[i] << ' ';
+        cout << cnt[i] << ' ';
     }
 }
