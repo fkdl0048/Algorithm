b77ef1fc0d0859f51d55c21594d4f267c7751781
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2024-10-09 03:51:31 +0900

    Solve: Trapping Rain Water

---
+++ LeetCode/Trapping Rain Water.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if (height.empty()) return 0;
+
+        int left = 0, right = height.size() - 1;
+        int left_max = 0, right_max = 0;
+        int total_water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= left_max) {
+                    left_max = height[left];
+                } else {
+                    total_water += left_max - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= right_max) {
+                    right_max = height[right];
+                } else {
+                    total_water += right_max - height[right];
+                }
+                right--;
+            }
+        }
+
+        return total_water;
+    }
+};
