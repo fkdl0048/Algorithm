93685fdb0b18a598708176c8f5bd71e9778a3ec1
fkdl0048 <fkdl000048@gmail.com>
2024-10-12 13:17:19 +0900

    Solve: Product of Array Except Self

---
+++ LeetCode/Product of Array Except Self.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> output(nums.size(), 1);
+
+        int left = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            output[i] *= left;
+            left *= nums[i];
+        }
+
+        int right = 1;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
+        }
+
+        return output;
+    }
+};
