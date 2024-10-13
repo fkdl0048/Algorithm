32f27b722fb3b8554224c8e92ced4396fa1dcc50
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2024-10-13 23:37:28 +0900

    Solve: Container With Most Water 2nd

--- LeetCode/Container With Most Water.cpp
+++ LeetCode/Container With Most Water.cpp
@@ -17,3 +17,33 @@ public:
         return maxArea;
     }
 };
+
+// 2회차 풀이
+
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left =0;
+        int right = height.size() - 1;
+        int max = 0;
+        int current = 0;
+
+        while (left < right) {
+            int wall = std::min(height[left], height[right]);
+            current = wall * (right - left);
+            
+            if (current > max) {
+                max = current;
+            }
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return max;
+    }
+};
