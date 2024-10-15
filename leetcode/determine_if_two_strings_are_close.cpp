4f7d23dce76415fb870932f98fcc29989f06cbfd
GitHub Action <action@github.com>
2024-10-15 07:08:02 +0000

    이슈 #90에서 솔루션 추가

---
+++ leetcode/determine_if_two_strings_are_close.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if (word1.size() != word2.size()) {
+            return false;
+        }
+
+        vector<int> freq1(26, 0), freq2(26, 0);
+        vector<int> exists1(26, 0), exists2(26, 0);
+
+        for (char c : word1) {
+            freq1[c - 'a']++;
+            exists1[c - 'a'] = 1;
+        }
+
+        for (char c : word2) {
+            freq2[c - 'a']++;
+            exists2[c - 'a'] = 1;
+        }
+
+        if (exists1 != exists2) {
+            return false;
+        }
+
+        sort(freq1.begin(), freq1.end());
+        sort(freq2.begin(), freq2.end());
+
+        return freq1 == freq2;
+    }
+};
