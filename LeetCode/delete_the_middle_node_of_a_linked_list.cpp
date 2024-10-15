130accca6febe1144d9f58cee2011a8088eabbf9
GitHub Action <action@github.com>
2024-10-15 06:47:00 +0000

    이슈 #97에서 LeetCode 솔루션 추가

---
+++ LeetCode/delete_the_middle_node_of_a_linked_list.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return nullptr;
+        }
+
+        ListNode* fast = head;
+        ListNode* slow = head;
+        ListNode* prev = nullptr;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            prev = slow;
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        if (prev != nullptr) {
+            prev->next = slow->next;
+        }
+
+        return head;
+    }
+};
