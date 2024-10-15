757db04ca747ca6af9e61258fb6b3c78f2cfb603
GitHub Action <action@github.com>
2024-10-15 06:46:13 +0000

    이슈 #89에서 LeetCode 솔루션 추가

---
+++ LeetCode/unique_number_of_occurrences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> countMap;
+        unordered_set<int> occurrenceSet; 
+
+        for (int num : arr) {
+            countMap[num]++;
+        }
+
+        for (auto& pair : countMap) {
+            if (occurrenceSet.find(pair.second) != occurrenceSet.end()) {
+                return false;
+            }
+
+            occurrenceSet.insert(pair.second);
+        }
+
+        return true;
+    }
+};
