aa09d592e1be7ea9c152a6a309780394d0494d4c
GitHub Action <action@github.com>
2024-10-15 06:46:25 +0000

    이슈 #91에서 LeetCode 솔루션 추가

---
+++ LeetCode/equal_row_and_column_pairs.cpp
@@ -0,0 +1,25 @@
+using namespace std;
+
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        map<vector<int>, int> m;
+        int n = grid.size();
+        int result = 0;
+
+        for (auto& row : grid) {
+            m[row]++;
+        }
+
+        for (int col = 0; col < n; col++) {
+            vector<int> temp;
+            for (int row = 0; row < n; row++) {
+                temp.push_back(grid[row][col]);
+            }
+
+            result += m[temp];
+        }
+
+        return result;
+    }
+};
