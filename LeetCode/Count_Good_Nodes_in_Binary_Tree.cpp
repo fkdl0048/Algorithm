694ef4f5a603bed390233db91fba66b23824f3ca
GitHub Action <action@github.com>
2024-10-17 05:51:58 +0000

    이슈 #114에서 솔루션 추가

---
+++ LeetCode/Count_Good_Nodes_in_Binary_Tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        return countGoodNodes(root, root->val);
+    }
+
+private:
+    int countGoodNodes(TreeNode* node, int maxSoFar) {
+        if (node == nullptr) {
+            return 0;
+        }
+
+        int goodNodeCount = (node->val >= maxSoFar) ? 1 : 0;
+
+        maxSoFar = max(maxSoFar, node->val);
+
+        goodNodeCount += countGoodNodes(node->left, maxSoFar);
+        goodNodeCount += countGoodNodes(node->right, maxSoFar);
+
+        return goodNodeCount;
+    }
+};
