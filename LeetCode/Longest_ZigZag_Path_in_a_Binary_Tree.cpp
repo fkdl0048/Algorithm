b7c8fe4af07ebd5e38a4c27007c7975ff96642f0
GitHub Action <action@github.com>
2024-10-17 06:47:21 +0000

    이슈 #116에서 솔루션 추가

---
+++ LeetCode/Longest_ZigZag_Path_in_a_Binary_Tree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int longestZigZag(TreeNode* root) {
+        int maxLength = 0;
+        dfs(root, true, 0, maxLength);
+        dfs(root, false, 0, maxLength);
+        return maxLength;
+    }
+
+private:
+    void dfs(TreeNode* node, bool isLeft, int currentLength , int& maxLength) {
+        if (node == nullptr) {
+            return;
+        }
+
+        maxLength = max(currentLength, maxLength);
+
+        if (isLeft) {
+            dfs(node->left, false, currentLength + 1, maxLength);
+            dfs(node->right, true, 1, maxLength);
+        }
+        else {
+            dfs(node->right, true, currentLength + 1, maxLength);
+            dfs(node->left, false, 1, maxLength);
+        }
+    }
+};
