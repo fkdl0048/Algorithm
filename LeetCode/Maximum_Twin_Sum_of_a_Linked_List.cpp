505f3dbdc2cc81852e28d525278470a24e3601c6
GitHub Action <action@github.com>
2024-10-17 04:30:31 +0000

    이슈 #111에서 솔루션 추가

---
+++ LeetCode/Maximum_Twin_Sum_of_a_Linked_List.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        ListNode* prev = nullptr;
+        while (slow != nullptr) {
+            ListNode* nextTemp = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextTemp;
+        }
+
+        int maxSum = 0;
+        ListNode* start = head;
+        while(prev != nullptr) {
+            maxSum = max(maxSum, prev->val + start->val);
+            prev = prev->next;
+            start = start->next;
+        }
+        
+        return maxSum;
+    }
+};
