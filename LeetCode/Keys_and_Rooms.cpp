4fadd992921506acf5b8f9a3053f0d081c3426da
GitHub Action <action@github.com>
2024-10-19 13:17:19 +0000

    이슈 #123에서 솔루션 추가

---
+++ LeetCode/Keys_and_Rooms.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<bool> visited(n, false);
+        stack<int> s;
+        s.push(0);
+        visited[0] = true;
+
+        while (!s.empty()) {
+            int curr = s.top();
+            s.pop();
+            for (int key : rooms[curr]) {
+                if (!visited[key]) {
+                    visited[key] = true;
+                    s.push(key);
+                }
+            }
+        }
+
+        for (bool v : visited) {
+            if (!v) return false;
+        }
+
+        return true;
+    }
+};
