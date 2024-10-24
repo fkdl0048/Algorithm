1c4f56b25eb8ac868b322da63d298e5f58a46bb5
GitHub Action <action@github.com>
2024-10-24 07:38:51 +0000

    이슈 #130에서 솔루션 추가

---
+++ LeetCode/Smallest_Number_in_Infinite_Set.cpp
@@ -0,0 +1,29 @@
+class SmallestInfiniteSet {
+public:
+    SmallestInfiniteSet() {
+        constexpr int N = 1000;
+        for (int i = 1; i <= N; i++) {
+            s.emplace(i);
+        }
+    }
+    
+    int popSmallest() {
+        const int res = *s.begin();
+        s.erase(s.begin());
+        return res;
+    }
+    
+    void addBack(int num) {
+        s.emplace(num);
+    }
+
+private:
+    set<int> s;
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
