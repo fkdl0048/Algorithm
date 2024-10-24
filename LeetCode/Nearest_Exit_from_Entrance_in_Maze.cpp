efbfa7b9633a1e80c040d7fe1b5e3758d9735359
GitHub Action <action@github.com>
2024-10-24 06:43:43 +0000

    이슈 #127에서 솔루션 추가

---
+++ LeetCode/Nearest_Exit_from_Entrance_in_Maze.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        int row = maze.size();
+        int col = maze[0].size();
+        int steps = 0;
+
+        vector<vector<bool>> visited(row, vector<bool>(col, false));
+        queue<pair<int, int>> q;
+
+        int dirs[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        int startX = entrance[1];
+        int startY = entrance[0];
+
+        visited[startY][startX] = true;
+        q.push({startY, startX});
+
+        while(!q.empty()) {
+            int n = q.size();
+            steps++;
+
+            for (int i = 0; i < n; i++) {
+                auto[y, x] = q.front();
+                q.pop();
+
+                for (auto& dir : dirs) {
+                    int nx = x + dir[1];
+                    int ny = y + dir[0];
+
+                    if (nx >= 0 && nx < col && ny >=0 && ny < row 
+                    && maze[ny][nx] == '.' && visited[ny][nx] == false) {
+                        if (nx == 0 || nx == col - 1 || ny == 0 || ny == row - 1) {
+                            if (!(nx == startX && ny == startY)) {
+                                return steps;
+                            }
+                        }
+
+                        visited[ny][nx] = true;
+                        q.push({ny, nx});
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+};
