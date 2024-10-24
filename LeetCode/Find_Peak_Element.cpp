b1e758c8abbc790142568bcd174528611c5b2e0f
GitHub Action <action@github.com>
2024-10-24 14:51:44 +0000

    이슈 #135에서 솔루션 추가

---
+++ LeetCode/Find_Peak_Element.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+class Solution {
+public:
+    int findPeakElement(std::vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+    
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+    
+            if (nums[mid] > nums[mid + 1]) {
+                // 왼쪽에 피크가 있음
+                right = mid;
+            } else {
+                // 오른쪽에 피크가 있음
+                left = mid + 1;
+            }
+        }
+    
+        return left;
+    }
+};
