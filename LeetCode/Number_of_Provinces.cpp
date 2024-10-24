351dc7bea2017ca19a0954f316a9e4f36fea85e4
GitHub Action <action@github.com>
2024-10-24 05:44:06 +0000

    이슈 #124에서 솔루션 추가

---
+++ LeetCode/Number_of_Provinces.cpp
@@ -0,0 +1,31 @@
+// dfs로 풀이,
+// n개까지 반복하는데, 방분하지 않은 곳일 경우 dfs를 타서 방문 노드를 찍음
+// 이 반복에서 provinces를 증감하면 됨
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int provinces = 0;
+        int n = isConnected.size();
+        vector<bool> visited(n, false);
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == false) {
+                dfs(isConnected, visited, i);
+                provinces++;
+            }
+        }
+
+        return provinces;
+    }
+private:
+    void dfs(const vector<vector<int>>& isConnected, vector<bool>& visited, int city) {
+        visited[city] = true;
+
+        for (int i = 0; i < isConnected.size(); i++) {
+            if (isConnected[city][i] == 1 && visited[i] == false) {
+                dfs(isConnected, visited, i);
+            }
+        }
+    }
+};
