0506470f6789e627ff06f76f688cbaf89255ed54
GitHub Action <action@github.com>
2024-10-24 14:47:41 +0000

    이슈 #134에서 솔루션 추가

---
+++ LeetCode/Successful_Pairs_of_Spells_and_Potions.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        int m = potions.size();
+        vector<int> result;
+
+        for (int spell : spells) {
+            long long minPotion = (success + spell - 1) / spell;
+            int index = lowerBound(potions, minPotion);
+
+            result.push_back(m - index);
+        }
+
+        return result;
+    }
+
+private:
+    int lowerBound(const std::vector<int>& potions, long long target) {
+        int left = 0;
+        int right = potions.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (potions[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+};
