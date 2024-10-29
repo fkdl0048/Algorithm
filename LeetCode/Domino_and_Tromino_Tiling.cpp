818d0c87fe1bac9bca84ff4d711c2d24f4db56cd
GitHub Action <action@github.com>
2024-10-29 11:31:35 +0000

    이슈 #142에서 솔루션 추가

--- LeetCode/Domino_and_Tromino_Tiling.cpp
+++ LeetCode/Domino_and_Tromino_Tiling.cpp
@@ -1,20 +1,22 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int m = text1.size();
-        int n = text2.size();
-        vector<vector<int>> dp(m + 1, vector(n + 1, 0));
+    int numTilings(int n) {
+        const int MOD = 1e9 + 7;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; ++i) {
+            dp[i] = ((dp[i - 1] + dp[i - 2]) % MOD + (2 * dp[i - 3]) % MOD) % MOD;
         }
 
-        return dp[m][n];
+        return dp[n];
     }
 };
