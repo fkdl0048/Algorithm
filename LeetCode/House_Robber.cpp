c50e73dcbb71c958853e6e2a7f7f24f93609e6cc
GitHub Action <action@github.com>
2024-10-29 05:16:45 +0000

    이슈 #141에서 솔루션 추가

---
+++ LeetCode/House_Robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        int prev2 = 0;
+        int prev1 = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            int curr = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+};
