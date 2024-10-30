af4a2f14c753efc2ca1da6f631ae2ac38a69c99f
GitHub Action <action@github.com>
2024-10-30 18:28:11 +0000

    이슈 #161에서 솔루션 추가

---
+++ Programmers/a와_b_출력하기.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        int b = Int32.Parse(s[1]);
+
+        Console.WriteLine($"a = {a}");
+        Console.WriteLine($"b = {b}");
+    }
+}
