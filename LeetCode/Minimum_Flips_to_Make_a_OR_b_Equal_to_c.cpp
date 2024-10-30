55a2908dd7f8c183a1a7e7c448637a12470cb49c
GitHub Action <action@github.com>
2024-10-30 17:54:32 +0000

    이슈 #149에서 솔루션 추가

---
+++ LeetCode/Minimum_Flips_to_Make_a_OR_b_Equal_to_c.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int flips = 0;
+        for (int i = 0; i < 32; ++i) {
+
+            int bit_a = (a >> i) & 1;
+            int bit_b = (b >> i) & 1;
+            int bit_c = (c >> i) & 1;
+            
+            if (bit_c == 0) {
+                flips += bit_a + bit_b;
+            } else {
+                if (bit_a == 0 && bit_b == 0) ++flips;
+            }
+        }
+        return flips;
+    }
+};
