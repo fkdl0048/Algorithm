ec7651a3ed5893e79bbcd3e172646728e7f3de75
GitHub Action <action@github.com>
2024-11-05 04:11:19 +0000

    이슈 #167에서 솔루션 추가

---
+++ Programmers/덧셈식_출력하기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        int b = Int32.Parse(s[1]);
+
+        Console.WriteLine($"{a} + {b} = {a + b}");
+    }
+}
