5807199fae8073207626ecbc55e3cf1dc8110d02
GitHub Action <action@github.com>
2024-11-05 10:53:33 +0000

    이슈 #174에서 솔루션 추가

---
+++ LeetCode/Merge_Sorted_Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int midx = m - 1;
+        int nidx = n - 1;
+        int right = m + n - 1;
+
+        while (nidx >= 0) {
+            if (midx >= 0 && nums1[midx] > nums2[nidx]) {
+                nums1[right] = nums1[midx];
+                midx--;
+            } else {
+                nums1[right] = nums2[nidx];
+                nidx--;
+            }
+            right--;
+        }
+    }
+};
