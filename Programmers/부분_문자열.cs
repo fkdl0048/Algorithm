2da5957364186570d2b1df02ef87b4c0f608dd6e
GitHub Action <action@github.com>
2024-11-05 04:09:33 +0000

    이슈 #159에서 솔루션 추가

--- Programmers/부분_문자열.cs
+++ Programmers/부분_문자열.cs
@@ -1,9 +1,15 @@
-using System;
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-public class Example
-{
-    public static void Main()
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* str1, const char* str2) {
+    if (strstr(str2, str1) != NULL) 
     {
-        Console.WriteLine("!@#$%^&*(\\\'\"<>?:;");
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
 }
