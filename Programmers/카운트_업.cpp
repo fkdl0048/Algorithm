3cd267d873d6f6b1b7cb8db4fb46202671b015a8
GitHub Action <action@github.com>
2024-11-12 06:08:34 +0000

    이슈 #225에서 솔루션 추가

---
+++ Programmers/카운트_업.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int start_num, int end_num) {
+    vector<int> answer;
+    
+    while (start_num <= end_num) {
+        answer.push_back(start_num++);
+    }
+    
+    return answer;
+}
