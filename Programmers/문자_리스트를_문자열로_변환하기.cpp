39940d4ffa9e137d145d349912af20bcba58736b
GitHub Action <action@github.com>
2024-11-12 07:00:15 +0000

    이슈 #232에서 솔루션 추가

---
+++ Programmers/문자_리스트를_문자열로_변환하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> arr) {
+    string answer = "";
+    for (auto c : arr) {
+        answer += c;
+    }
+    return answer;
+}
