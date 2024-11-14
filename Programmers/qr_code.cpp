fd28c9d8d9b6c81e653060d1fe6c78150bc79a7f
GitHub Action <action@github.com>
2024-11-14 23:08:16 +0000

    이슈 #277에서 솔루션 추가

---
+++ Programmers/qr_code.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int q, int r, string code) {
+    string answer = "";
+    
+    for (int i = 0; i < code.length(); i++) {
+        if ((i % q) == r) {
+            answer += code[i];
+        }
+    }
+    
+    return answer;
+}
