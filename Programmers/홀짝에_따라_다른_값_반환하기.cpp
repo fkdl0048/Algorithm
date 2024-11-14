f540bbe414bd99a76570b1db4680cd46e63885fc
GitHub Action <action@github.com>
2024-11-14 03:52:22 +0000

    이슈 #238에서 솔루션 추가

---
+++ Programmers/홀짝에_따라_다른_값_반환하기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    if (n % 2 == 0) {
+        for (int i = 0; i <= n; i += 2) {
+            answer += i * i;
+        }
+    } else {
+        for (int i = 1; i <= n; i += 2) {
+            answer += i;
+        }
+    }
+    
+    return answer;
+}
