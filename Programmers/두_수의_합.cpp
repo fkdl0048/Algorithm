bbfec0e7b90f1df8778ce54dfe5a5bf690a6a3b8
GitHub Action <action@github.com>
2024-11-19 06:29:50 +0000

    이슈 #302에서 솔루션 추가

--- Programmers/두_수의_합.cpp
+++ Programmers/두_수의_합.cpp
@@ -1,32 +1,8 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-string solution(string a, string b) {
-    string answer = "";
-    int carry = 0;
-    int i = a.length() - 1;
-    int j = b.length() - 1;
-    
-    while (i >= 0 || j >= 0 || carry) {
-        int sum = carry;
-        
-        if (i >= 0) {
-            sum += a[i] - '0';
-            i--;
-        }
-        if (j >= 0) {
-            sum += b[j] - '0';
-            j--;
-        }
-        
-        carry = sum / 10;
-        answer += (sum % 10) + '0';
-    }
-    
-    reverse(answer.begin(), answer.end());
-    
-    return answer;
+int solution(int num1, int num2) {
+    return num1 + num2;
 }
