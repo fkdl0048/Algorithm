6d43ed98e44d6645203f8ecba70cf0a3a0eb0053
GitHub Action <action@github.com>
2024-12-05 03:27:08 +0000

    이슈 #446에서 솔루션 추가

---
+++ LeetCode/Jump_Game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (maxReach < i) {
+                return false;
+            }
+
+            maxReach = max(maxReach, i + nums[i]);
+
+            if (maxReach >= nums.size() - 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
