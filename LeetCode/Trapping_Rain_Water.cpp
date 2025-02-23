12391f3c36139673a0e6a187c934ae71eb336116
GitHub Action <action@github.com>
2025-02-23 01:37:46 +0000

    이슈 #453에서 솔루션 추가

---
+++ LeetCode/Trapping_Rain_Water.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int left_max = height[left];
+        int right_max = height[right];
+        int water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= left_max) {
+                    left_max = height[left];
+                } else {
+                    water += left_max - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= right_max) {
+                    right_max = height[right];
+                } else {
+                    water += right_max - height[right];
+                }
+                right--;
+            }
+        }
+
+        return water;
+    }
+};
